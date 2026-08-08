3510b003ffd6a640df2fbdad71602d0c8eaac77b
dseynabou774-debug <dseynabou774@gmail.com>
2026-08-08 07:39:49 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
 // rafraîchissement du cache sur les téléphones déjà installés.
 // =============================================================================
 
-const CACHE_NAME = "sza-cache-v69";
+const CACHE_NAME = "sza-cache-v70";
 
 const CORE_ASSETS = [
   "./",
